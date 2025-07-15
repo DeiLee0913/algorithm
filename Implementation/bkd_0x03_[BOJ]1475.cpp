72e7f9804c820a76301553b527ba759f5021216d
D <deilee0913@gmail.com>
2025-07-15 18:09:54 +0900

    solve: #1 bkd_[BOJ]1475, 1919

---
+++ Implementation/bkd_0x03_[BOJ]1475.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	// 0~9 각 숫자의 등장 횟수 저장
+	int cnt[10] = {};
+
+	// 각 자리수 숫자 세기
+	while (n > 0) {
+		cnt[n % 10]++;
+		n /= 10;
+	}
+	
+	// 6과 9는 서로 대체 가능 → 합쳐서 올림 평균 내기
+	//cnt[6] = (cnt[6] + cnt[9]) / 2 + (cnt[6] + cnt[9]) % 2;
+	cnt[6] = (cnt[6] + cnt[9] + 1) / 2;
+	cnt[9] = 0;
+
+	int max = 0;
+	// 0~8 중 가장 많이 등장한 숫자 개수 찾기
+	for (int i = 0; i < 9; i++) {
+		if (max < cnt[i]) {
+			max = cnt[i];
+		}
+	}
+
+	// 필요한 세트 수 출력
+	cout << max << "\n";
+
+	return 0;
+}
---
+++ Implementation/bkd_0x03_[BOJ]1919.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string word1, word2;
+	cin >> word1 >> word2;
+
+	char occur1[26] = {};
+	char occur2[26] = {};
+
+	// 첫 번째 단어 알파벳 개수 기록
+	for (char c : word1) {
+		occur1[c - 'a']++;
+	}
+
+	// 두 번째 단어 알파벳 개수 기록
+	for (char c : word2) {
+		occur2[c - 'a']++;
+	}
+
+	int ans = 0;
+
+	// 각 알파벳별 차이 누적
+	for (int i = 0; i < 26; i++) {
+		ans += abs(occur1[i] - occur2[i]);
+	}
+	
+	cout << ans;
+
+	return 0;
+}
